fc94e41142341f8baa7b291a77e622dc6c26be8b
devloper <dimlightray@gmail.com>
2018-11-29 20:18:06 +0900

    create blockchain action

--- src/p2p.js
+++ src/p2p.js
@@ -1,6 +1,37 @@
-const WebSockets = require("ws");
+const WebSockets = require("ws"),
+Blockchain = require("./blockchain");
+
+
+const { getLastBlock } = Blockchain;
 
 const sockets = [];
+
+// Message Types
+const GET_LATEST = "GET_LATEST";
+const GET_ALL = "GET_ALL";
+const BLOCKCHAIN_RESPONSE = "BLOCKCHAIN_RESPONSE";
+
+// Message Creators
+const getLatest = () => {
+    return {
+        type: GET_LATEST,
+        data: null
+    };
+};
+
+const getAll = () => {
+    return {
+        type: GET_ALL,
+        data: null
+    };
+};
+
+const blockchainResponse = (data) => {
+    return {
+        type: BLOCKCHAIN_RESPONSE,
+        data
+    };
+};
 
 const getSockets = () => sockets
 
@@ -12,16 +43,38 @@ const startP2PServer = server => {
     console.log("Lunarcoin P2P Server is runnning");
 }; 
 
-const initSocketConnection = socket => {
-    sockets.push(socket);
-    handleSocketError(socket);
-    socket.on("message", (data) => {
-        console.log(data);
-    })
-    setTimeout(() => { 
-        socket.send("welcome");
-    }, 5000);
+const initSocketConnection = ws => {
+    sockets.push(ws);
+    handleSocketMessages(ws);
+    handleSocketError(ws);
+    sendMessaage(ws, getLatest());
 };
+
+const parseData = data => {
+    try {
+      return JSON.parse(data)
+    } catch(e) {
+        console.log(e)
+        return null;
+    }
+}
+
+const handleSocketMessages = ws => {
+    ws.on("message", data => {
+        const message = parseData(data);
+        if(message === null){
+            return;
+        }
+        console.log(message);
+        switch(message.type){
+            case GET_LATEST:
+                sendMessaage(ws, getLastBlock());
+                break;
+        }
+    });
+};
+
+const sendMessaage = (ws, message) => ws.send(JSON.stringify(message));
 
 const handleSocketError = ws => {
     const closeSocketConnection = ws => {
